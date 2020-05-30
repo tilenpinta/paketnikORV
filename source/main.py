f7de16ebb54574d15242eea15ea2ac3d9bcf52b6
hp-cx0003nm <ivan.jovanovitsh.mb@gmail.com>
2020-05-30 20:18:54 +0200

    Dokumentacija

--- source/main.py
+++ source/main.py
@@ -3,49 +3,38 @@ import cv2
 import _pickle as pickle
 import requests
 
-faceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-faceRecognizer = cv2.face.LBPHFaceRecognizer_create()
-faceRecognizer.read("trainner.yml")
+faceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml') # za detekcijo sprednjega dela obraza
+faceRecognizer = cv2.face.LBPHFaceRecognizer_create() # tega ne smemo uporabiti
+faceRecognizer.read("trainner.yml") # ne smemo uporabiti, drugače pa read() prebere naš model, katerega smo naučili na podlagi slik (znotraj train.py skripte)
 
 
-labelDictionary = {}
+labelDictionary = {}  # direktorije labeliramo, ta labela je slovar (HashMap, Dictionary), ki je sestavljen iz id(ključ) na katerem se nahaja ime direktorija
+                      #(v našem primeru so imena direktorij pač imena oseb v source/images)
 
 with open("labels.pickle", 'rb') as file:
     tmpLabels = pickle.load(file)
-    labelDictionary = {v:k for k,v  in tmpLabels.items()} # obrnemo
+    labelDictionary = {v:k for k,v  in tmpLabels.items()} # napolnimo slovar
 
-capture = cv2.VideoCapture(0) # za zajemanje videa 
+capture = cv2.VideoCapture(0, cv2.CAP_DSHOW) # za zajemanje videa 
 
 while(True):
-    #images = requests.get("http://192.168.0.11:8080/shot.jpg")
-    #video = np.array(bytearray(images.content), dtype=np.uint8)
-    #render = cv2.imdecode(video, -1)
     ret, frame = capture.read()
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    facesArray = faceCascade.detectMultiScale(grayImage, scaleFactor=1.5, minNeighbors=5)
+    facesArray = faceCascade.detectMultiScale(grayImage, scaleFactor=1.5, minNeighbors=5) # za detekcijo obraza (6 vrstica v tej skripti)
 
     for (x, y, w, h) in facesArray:
-        #print(x,y,w,h)                   
-        regionOfInterestGray = grayImage[y:y+h, x:x+w]
-        regionOfInterestBGR = frame[y:y+h, x:x+w] 
+        regionOfInterestGray = grayImage[y:y+h, x:x+w]  # y je začetna točka, y+h pa končna točka vertikalne črte. x je začetna točka, x+w pa končna točka horizontalne črte
         
-        id_, conf = faceRecognizer.predict(regionOfInterestGray)
+        id_, conf = faceRecognizer.predict(regionOfInterestGray) # ne smemo uporabiti, moramo implementirati
         
-        if (conf > 100):
+        if (conf > 15 and conf <50):     # če je ocena v redu (med 15 in 50), potem sprejmi id kot veljaven (id je ključ, s katerim najdemo ime osebe v labelDictionary)
             print(labelDictionary[id_])
 
-       # imgItem = "ivan.png"
-        #cv2.imwrite(imgItem, regionOfInterestGray)
-
-        # s tem izrišemo pravokotnik znotraj katerega je ROI
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 255), 3) # zacetne in koncne koordinate x, y ter x+w in y+h
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 255), 3) # zacetne in končne koordinate x, y ter x+w in y+h
 
     cv2.imshow('Slika', frame)
-    if (cv2.waitKey(27) & 0xFF == ord('q')):
+    if (cv2.waitKey(20) & 0xFF == ord('q')):
         break
 
 capture.release()
 cv2.destroyAllWindows()
-
-
-#print(cv2.__file__)
--- source/train.py
+++ source/train.py
@@ -3,43 +3,45 @@ import numpy as np
 import cv2
 import _pickle as pickle
 
-baseDir = os.path.dirname(os.path.abspath(__file__))
-imageDir = os.path.join(baseDir, "images")
-faceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-faceRecognizer = cv2.face.LBPHFaceRecognizer_create()
+baseDir = os.path.dirname(os.path.abspath(__file__)) #pot do direktorija "source"
+imageDir = os.path.join(baseDir, "images") # tej poti dodamo še "images", dobimo pot do direktorija "images"
+
+faceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml') # za detekcijo sprednjega dela obraza
+faceRecognizer = cv2.face.LBPHFaceRecognizer_create() # ne smemo uporabit, drugače pa funkcija train() izlušči značilnice, katere s pomočjo save() shranimo v .yml datoteko 
+                                                      # na podlagi teh značilnic opravimo razpoznavo 
 
 labelArray = [] # y_labels
 labelDictionary = {} #label_ids
 trainArray = [] # x_train 
 currentId = 0
 
-# sprehajamo se znotraj zbirke images
-# in preverjamo vsako datoteko vanjo
+# sprehajamo se znotraj direktorija images
+# in preverjamo vsako datoteko vanj
 for root, dirs, files in os.walk(imageDir):
     for file in files:
-        if file.endswith("jpg") or file.endswith("jpg"):
-            path = os.path.join(root, file) # dobimo pot do slike, ki ima končnico .jpg ali .png
+        if file.endswith("jpg") or file.endswith("png") or file.endswith("jpeg"): # sprejeti formati za branje slike so jpeg, jpg, png  
+            path = os.path.join(root, file) # dobimo pot do slike 
             label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower() #dobimo direktorij od slike 
             
-            # label za vsako osebo, ki jo poznamo 
+            # če osebo(ime direktorija) še ne poznamo, ga dodamo v slovar 
             if(not label in labelDictionary):         
                 labelDictionary[label] = currentId
                 currentId += 1
                   
-            id_ = labelDictionary[label]
+            id_ = labelDictionary[label] # v id_ shranimo ime direktorija(osebe)
         
-            imageArray = cv2.imread(path, 0)
+            imageArray = cv2.imread(path, 0) # preberemo sliko, do katere smo pridobili pot
             
-            faces = faceCascade.detectMultiScale(imageArray, scaleFactor=1.5, minNeighbors=5)
+            faces = faceCascade.detectMultiScale(imageArray, scaleFactor=1.5, minNeighbors=5) # iz prebrane slike (imageArray) vzamemo roi(regionOfInterest) oz. v našem primeru obraz osebe
             
             for(x,y,w,h) in faces:
                 regionOfInterest = imageArray[y:y+h, x:x+w]
-                trainArray.append(regionOfInterest)
-                labelArray.append(id_)
+                trainArray.append(regionOfInterest) # polje regionOfInteres (obraz), dodamo v polje za učenje modela
+                labelArray.append(id_) # dodamo še id direktorija(osebe)
 
 with open("labels.pickle", 'wb') as file:
-    pickle.dump(labelDictionary, file)
+    pickle.dump(labelDictionary, file) #shranimo slovar, ki vsebuje id in ime direktorija(osebe)
 
 
-faceRecognizer.train(trainArray, np.array(labelArray))
-faceRecognizer.save("trainner.yml")
+faceRecognizer.train(trainArray, np.array(labelArray)) # učimo naš model
+faceRecognizer.save("trainner.yml") #shranimo, da lahko potem v main.py preberemo
